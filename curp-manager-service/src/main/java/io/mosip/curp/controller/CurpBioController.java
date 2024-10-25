c5589f8ed90c069c02093be91855e18ba4954772
Muralitharan K <muralitharan@mosip.io>
2024-10-25 18:19:22 +0530

    Logs added

--- curp-manager-service/src/main/java/io/mosip/curp/controller/CurpBioController.java
+++ curp-manager-service/src/main/java/io/mosip/curp/controller/CurpBioController.java
@@ -1,5 +1,7 @@
 package io.mosip.curp.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.mosip.curp.dto.MatchedCurpDto;
 import io.mosip.curp.helper.CurpBioHelper;
 import io.mosip.curp.helper.CurpManagerLogger;
@@ -21,21 +23,35 @@ public class CurpBioController {
     @Autowired
     private CurpBioHelper curpBioHelper;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @PostMapping("/updatematch")
-    public ResponseEntity<String> updateCurpBioData(@RequestBody @Valid MatchedCurpDto matchedCurpDto) {
-        LOGGER.info("CurpBioController::updateCurpBioData entry");
-        String response = curpBioHelper.updateCurpBioData(matchedCurpDto);
-        if (response.equals("CurpBioData and MatchedCurp updated successfully")) {
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<String> updateCurpBioData(@RequestBody @Valid MatchedCurpDto matchedCurpDto) throws JsonProcessingException {
+        try {
+            LOGGER.info("CurpBioController::updateCurpBioData entry, request {}", mapper.writeValueAsString(matchedCurpDto));
+            String response = curpBioHelper.updateCurpBioData(matchedCurpDto);
+            if (response.equals("CurpBioData and MatchedCurp updated successfully")) {
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (Exception ex) {
+            LOGGER.error("Exception on updateCurpBioData", ex);
+            throw ex;
         }
     }
 
     @PostMapping("/updatestatus")
-    public String updateCurpStatus(@RequestBody @Valid MatchedCurpDto matchedCurpDto) {
-        LOGGER.info("CurpBioController::updateCurpStatus entry");
-        return curpBioHelper.findAndUpdateCurpStatus(matchedCurpDto);
+    public String updateCurpStatus(@RequestBody @Valid MatchedCurpDto matchedCurpDto) throws JsonProcessingException {
+        try {
+
+            LOGGER.info("CurpBioController::updateCurpStatus entry, request: {}", mapper.writeValueAsString(matchedCurpDto));
+            return curpBioHelper.findAndUpdateCurpStatus(matchedCurpDto);
+        } catch (Exception ex) {
+            LOGGER.error("Exception on updateCurpStatus", ex);
+            throw ex;
+        }
     }
 
 }
